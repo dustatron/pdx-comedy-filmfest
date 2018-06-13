8acf14b82c90df92252f31faf9eb5e60d5a0ffed
Dusty McCord <dustymccord@gmail.com>
2018-06-13 06:00:57 +0000

    made active page tabs work.

---
+++ app/assets/javascripts/activepage.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('li.active').removeClass('active');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+
+});
+
+
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
-//= require jquery
 //= require bootstrap-sprockets
