a77f193c028da6127116ae224ebf67dd853978d3
Dusty McCord <dustymccord@gmail.com>
2018-06-19 05:20:54 +0000

    added login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,17 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   
+
+  
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/submit'
   get 'static_pages/login'
   
-
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   resources :users
   
