456f2b0da72f52afd1a1db8a4ff00c98fd2afadf
Dusty McCord <dustymccord@gmail.com>
2018-06-30 22:27:45 +0000

    added admin view and updated look.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,7 +2,8 @@ class SubmissionsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
     
     def index
-        @subs = Submission.all.paginate(page: params[:page])
+        @subs = Submission.all.paginate(page: params[:page], :per_page => 8)
+        @submissions = Submission.all.paginate(page: params[:page], :per_page => 8)
     end
     
     def new
@@ -14,14 +15,14 @@ class SubmissionsController < ApplicationController
         # @submit = current_user.Submission.build(submit_params)
         if @submit.save
             flash[:success] = "Your Video has been submitted"
-            redirect_to root_url
+            redirect_to current_user
         else
             render 'new'
         end
     end
     
     def show
-        @submission = Submission.find(params[:id]).paginate(page: params[:page])
+        @submission = Submission.find(params[:id])
         @user = User.find(@submission.user_id)
     end
     
