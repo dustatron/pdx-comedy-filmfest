f050340d29e49ba1fb31c2c358fac6ee6e57bca2
Dusty McCord <dustymccord@gmail.com>
2018-07-01 05:40:32 +0000

    added validation to model submissions.

--- app/models/sumbission.rb
+++ app/models/sumbission.rb
@@ -1,6 +1,10 @@
 class Sumbission < ApplicationRecord
     belongs_to :user
     validates :user_id, presence: true
+    validates :title, presence: true, length: { maximum: 50 }
+    validates :length, presence: true
+    validates :link, presence: true, length: { maximum: 50 }
+    validates :description, presence: true, length: { maximum: 250 }
     
 
 end
