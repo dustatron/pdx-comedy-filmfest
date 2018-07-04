adcff7ef599f697937085f2b30710c419d68942b
Dusty McCord <dustymccord@gmail.com>
2018-07-04 06:24:52 +0000

    added embeds to submissions.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,7 +3,7 @@ class Submission < ApplicationRecord
     validates :user_id, presence: true
     validates :title, presence: true, length: { maximum: 50 }
     validates :length, presence: true
-    validates :link, presence: true, length: { maximum: 50 }
+    validates :link, presence: true, length: { maximum: 200 }
     validates :description, presence: true, length: { maximum: 250 }
     
 
